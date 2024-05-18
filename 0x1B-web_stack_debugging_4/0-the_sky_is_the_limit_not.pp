ba42a488268d552b649d98ab04aadb6098f5e9a5
root <boachieawilliam@gmail.com>
2024-05-18 15:10:40 +0000

    updated

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,16 +1,13 @@
-#Puppet manifest fixes adjust the web stack so that it can handle 1000 request with 100 at a time
-# Increase the ULIMIT of the default file
+# This Puppet manifest configures Nginx to handle high concurrency with optimal settings
+
 exec { 'fix--for-nginx':
-	# Modify the ULIMIT value
-	command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-	# Specify the path for the sed command
-	path 	=> '/usr/local/bin/:/bin/',
+        # Modify the ULIMIT value
+        command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
+        # Specify the path for the sed command
+        path    => '/usr/local/bin/:/bin/',
 }
 
-# Restart Nginx
 exec { 'nginx-restart':
-	# Restart Nginx service
-	command => '/etc/init.d/nginx restart',
-	# Specify the path for the init.d script
-	path 	=> '/etc/init.d/',
+        command => '/etc/init.d/nginx restart',
+        path    => '/etc/init.d/',
 }
